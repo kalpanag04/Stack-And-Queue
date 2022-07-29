4a5a0015838660ebe602a1a6fc4ac4b246bde4ec
kalpanag04 <kalpanag808@gmail.com>
2022-07-29 17:04:27 +0530

    Ability Add elements in Queue

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using QueueUsingLinkedList;
+
+Console.WriteLine("Hello World!");
+QueueUsingLinkedList.LinkedListQueue linkedListQueue = new LinkedListQueue();
+linkedListQueue.Enqueue(56);
+linkedListQueue.Enqueue(30);
+linkedListQueue.Enqueue(70);
+linkedListQueue.Display();// this.top=this.top.next
+Console.ReadKey();
